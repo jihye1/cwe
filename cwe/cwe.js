182ac12e39f001fb27d5ec2a1f5dd05fa1818453
jihye1 <36979920+jihye1@users.noreply.github.com>
2019-10-10 11:56:10 +0900

    Update cwe.js

--- cwe/cwe.js
+++ cwe/cwe.js
@@ -1,1 +1,12 @@
-test
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
+function CWE_476() { // NULL_POINTER
+    var obj;
+    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
+    console.log(y);
+}
