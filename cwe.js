d845e39372e645a97fdbae17abac83c6b8cdb820
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-08-30 14:00:28 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -1,19 +1,5 @@
 // This will show examples related with the Common Weakness Enumeration (CWE).
 
-    function CWE_398() { // IDENTICAL_BRANCHES
-        if (x >= 0) {
-            y = x;
-        } else {
-            y = x;
-        }
-    }
-
-    function CWE_476() { // NULL_POINTER
-        var obj;
-        var y = obj.x;
-        console.log(y);
-    }
-
     function CWE_480() { // BAD_BITWISE_OPERATOR
         var obj = null;
         if (obj & obj.prop) {
