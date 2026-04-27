59e353ec82ba1180df0e6e5d931e046b778de58a
jihye1 <jihye.won1@samsung.com>
2026-04-27 19:57:22 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -1,12 +1,12 @@
 
 // This will show examples related with the Common Weakness Enumeration (CWE).
 
-function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
-    var arr = [1, 2, 3];
-    if (x < 0) {
-        arr[x] = 3;
-    }
-}
+// function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+//     var arr = [1, 2, 3];
+//     if (x < 0) {
+//         arr[x] = 3;
+//     }
+// }
 
 function CWE_563_2(url) { // UNUSED_VAR_ASSIGN
     var foo = url + "?query=deepscan";
